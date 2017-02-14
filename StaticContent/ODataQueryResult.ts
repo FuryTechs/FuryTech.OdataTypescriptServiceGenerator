58de8a44d6977ad7176e11baef096ffff86ddcf9
gallayl <lajos.gallay@digicpictures.com>
2017-02-14 15:42:49 +0100

    Add doc: ODataQuery, ODataQueryResult

--- StaticContent/ODataQueryResult.ts
+++ StaticContent/ODataQueryResult.ts
@@ -2,10 +2,16 @@ export class ODataQueryResult<T>{
     public '@odata.context': string;
     public '@odata.count': number;
 
+    /**
+     * The '@odata.context' variable returned by the OData service
+     */
     public get Context(): string {
         return this['@odata.context'];
     }
 
+    /**
+     * The '@odata.count' variable returned by the OData service
+     */
     public get Count(): number {
         return this['@odata.count'];
     }
