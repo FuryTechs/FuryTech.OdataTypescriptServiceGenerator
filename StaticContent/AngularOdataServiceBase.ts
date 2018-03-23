f712f207948922fc189629a8748c055741ca6b00
John Cannata <john@cannatasoftware.com>
2018-03-23 10:31:38 -0700

    Migrate from Http to HttpClient. Create OData query result instance from returned value.

--- StaticContent/AngularOdataServiceBase.ts
+++ StaticContent/AngularOdataServiceBase.ts
@@ -1,7 +1,5 @@
-import { RequestOptionsArgs } from '@angular/http/src/interfaces';
-import * as https from 'https';
 import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Response } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
 
 import { ODataGetOperation } from './ODataGetOperation';
@@ -15,113 +13,109 @@ import { ODataError } from './ODataError';
 
 export abstract class AngularODataServiceBase<T> extends ODataServiceAbstract<T> {
 
-    private requestOptions: RequestOptionsArgs = {
-        withCredentials: true
-    };
+  private requestOptions = {
+    headers: new HttpHeaders({
+      ['withCredentials']: 'true'
+    })
+  };
 
-    protected abstract entitySetUrlSegment: string;
+  protected abstract entitySetUrlSegment: string;
 
-    protected get entitySetUrl(): string {
-        return ODataContext.ODataRootPath + this.entitySetUrlSegment;
-    }
+  protected get entitySetUrl(): string {
+    return ODataContext.ODataRootPath + this.entitySetUrlSegment;
+  }
 
-    protected abstract http: Http;
-    constructor() {
-        super();
-    }
+  protected abstract http: HttpClient;
+  constructor() {
+    super();
+  }
 
-    public Get(id: any): ODataGetOperation<T> {
-        let idSegment = this.getEntityUriSegment(id);
-        return new ODataGetOperation<T>(idSegment, async (queryString: string) => {
-        return this.http.get(this.entitySetUrl + idSegment + queryString, {
-            withCredentials: true,
-        }).map(a => {
-            return a.json() as T;
-        }).toPromise();
+  public Get(id: any): ODataGetOperation<T> {
+    const idSegment = this.getEntityUriSegment(id);
+    return new ODataGetOperation<T>(idSegment, async (queryString: string) => {
+      return this.http.get(this.entitySetUrl + idSegment + queryString, {
+        withCredentials: true,
+      }).map(a => {
+        return a as T;
+      }).toPromise();
     });
+  }
+
+  private extractResponse(res: Response) {
+    if (res.status < 200 || res.status >= 300) {
+      throw new ODataError(res.status, res.json());
+    }
+    const body: any = res.json();
+    const entity: T = body;
+    return entity || null;
+  }
+
+  public async Post(entity: T): Promise<T> {
+    return this.http.post(this.entitySetUrl, entity, this.requestOptions)
+      .map(this.extractResponse)
+      .toPromise();
+  }
+
+  public async Patch(id: any, entity: T): Promise<any> {
+    return this.http
+      .patch(this.entitySetUrl + this.getEntityUriSegment(id), entity, this.requestOptions)
+      .toPromise();
+  }
+
+  public async Put(id: any, entity: T): Promise<T> {
+    return this.http
+      .put(this.entitySetUrl + this.getEntityUriSegment(id), entity, this.requestOptions)
+      .map(this.extractResponse)
+      .toPromise();
+  }
+
+  public async Delete(id: any): Promise<any> {
+    return this.http
+      .delete(this.entitySetUrl + this.getEntityUriSegment(id), this.requestOptions)
+      .toPromise();
+  }
+
+  protected async ExecCustomAction(actionName: string, entity: T, ...args: any[]): Promise<any> {
+    return this.http
+      .post(this.entitySetUrl + this.getEntityUriSegment(entity) + `/${actionName}`, null, this.requestOptions)
+      .toPromise();
+  }
+
+  protected async ExecCustomCollectionAction(actionName: string, ...args: any[]): Promise<any> {
+    return this.http
+      .post(this.entitySetUrl + actionName, null, this.requestOptions)
+      .toPromise();
+  }
+
+  protected async ExecCustomFunction(fucntionName: string, entity: T, ...args: any[]): Promise<any> {
+    return this.http
+      .get(this.entitySetUrl + this.getEntityUriSegment(entity) + `/${fucntionName}`, this.requestOptions)
+      .toPromise();
+  }
+
+  protected async ExecCustomCollectionFunction(fucntionName: string, ...args: any[]): Promise<any> {
+    return this.http
+      .get(this.entitySetUrl + fucntionName, this.requestOptions)
+      .toPromise();
+  }
+
+  Query(): ODataQuery<T> {
+    const http = this.http;
+    const entitySetUrl = this.entitySetUrl;
+
+    const evaluateQuery = (queryString: string): Promise<ODataQueryResult<T>> => {
+      const url = entitySetUrl + queryString;
+      const subscription = http.get(url, {
+        withCredentials: true
+      }).map(a => {
+        const result = new ODataQueryResult<T>();
+        Object.assign(result, a);
+        return result;
+      });
+
+      return subscription.toPromise();
     };
 
-    private extractResponse(res: Response) {
-        if (res.status < 200 || res.status >= 300) {
-            throw new ODataError(res.status, res.json());
-        }
-        let body: any = res.json();
-        let entity: T = body;
-        return entity || null;
-    }
-
-    public async Post(entity: T): Promise<T> {
-        return this.http.post(this.entitySetUrl, entity, this.requestOptions)
-            .map(this.extractResponse)
-            .toPromise();
-    }
-
-    public async Patch(id: any, entity: T): Promise<any> {
-        return this.http
-            .patch(this.entitySetUrl + this.getEntityUriSegment(id), entity, this.requestOptions)
-            .toPromise();
-    }
-
-    public async Put(id: any, entity: T): Promise<T> {
-        return this.http
-            .put(this.entitySetUrl + this.getEntityUriSegment(id), entity, this.requestOptions)
-            .map(this.extractResponse)
-            .toPromise();
-    }
-
-    public async Delete(id: any): Promise<any> {
-        return this.http
-            .delete(this.entitySetUrl + this.getEntityUriSegment(id), this.requestOptions)
-            .toPromise();
-    }
-
-    protected async ExecCustomAction(actionName: string, entity: T, ...args: any[]): Promise<any> {
-        return this.http
-            .post(this.entitySetUrl + this.getEntityUriSegment(entity) + `/${actionName}`, null, this.requestOptions)
-            .map(a => {return a.json(); } )
-            .toPromise();
-    }
-
-    protected async ExecCustomCollectionAction(actionName: string, ...args: any[]): Promise<any> {
-        return this.http
-            .post(this.entitySetUrl + actionName, null, this.requestOptions)
-            .map(a => {return a.json(); } )
-            .toPromise();
-    }
-
-    protected async ExecCustomFunction(fucntionName: string, entity: T, ...args: any[]): Promise<any> {
-        return this.http
-            .get(this.entitySetUrl + this.getEntityUriSegment(entity) + `/${fucntionName}`, this.requestOptions)
-            .map(a => {return a.json(); } )
-            .toPromise();
-    }
-
-    protected async ExecCustomCollectionFunction(fucntionName: string, ...args: any[]): Promise<any> {
-        return this.http
-            .get(this.entitySetUrl + fucntionName, this.requestOptions)
-            .map(a => {return a.json(); } )
-            .toPromise();
-
-    }
-
-
-    Query(): ODataQuery<T> {
-
-        let http = this.http;
-        let entitySetUrl = this.entitySetUrl;
-
-        let evaluateQuery = (queryString: string): Promise<ODataQueryResult<T>> => {
-            let url = this.entitySetUrl + queryString;
-            let subscription = http.get(url, {
-                withCredentials: true
-            }).map(a => {
-                return a.json() as ODataQueryResult<T>;
-            });
-
-            return subscription.toPromise();
-        };
-
-        return new ODataQuery(evaluateQuery);
-    }
-
+    return new ODataQuery(evaluateQuery);
+  }
 }
